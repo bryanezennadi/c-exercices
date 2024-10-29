234741a2b99a406b383a358dba2396f6fcb5d9cd
bryanezennadi <bryanezennadi@gmail.com>
2024-10-29 23:03:27 +0100

    esercizi per il 30 ottobre

---
+++ essocket/es8server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+#define DIM 512
+#define DimVettore 3
+#define SERVERPORT 1313
+void main()
+{
+    struct sockaddr_in servizio, addr_remoto;      // record con i dati del client e del server
+    int socketfd, soa, fromlen = sizeof(servizio); // identificatore socket
+
+    // settaggio socket locale
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
+    // creazione socket e transport endpoint
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    // impostazione del transport endpoint del server
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    // leghiamo l'indirizzo al transport endpoint
+    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    // poniamo il server in ascolto delle richieste dei client
+    listen(socketfd, 10); // può attendere fino a 10 client
+    // ciclo infinito
+
+    while (1)
+    {
+        printf("server in ascolto......\n");
+        fflush(stdout);
+        int vettore[DimVettore];
+
+        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, &fromlen);
+        // legge dal client
+        read(soa, vettore, DimVettore);
+        int max, min;
+        printf("vettore ricevuto\n");
+        for (int i = 0; i < DimVettore; i++)
+        {
+            if (vettore[i] > max)
+            {
+                max = vettore[i];
+            }else if(vettore[i]<min){
+                min= vettore[i];
+            }
+        }
+        char str2[DIM];
+        sprintf(str2, "il numero massimo è: %d \n il numero minimo è: %d \n ");
+        printf("Stringa inviata : %s\n", str2);
+        write(soa, str2, strlen(str2));
+        close(soa);
+    }
+    close(soa);
+}
